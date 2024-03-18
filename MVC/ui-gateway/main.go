b3887a498ac677b60cbee1e8211280de4c185080
Egor Kovalev <egorkowalev1997@gmail.com>
2024-03-18 19:25:56 +0300

    Test git

--- MVC/ui-gateway/main.go
+++ MVC/ui-gateway/main.go
@@ -11,6 +11,7 @@ import (
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, model.Album{})
 }
+
 func main() {
 	r := gin.Default()
 	db := model.SetupModels() // new
@@ -23,7 +24,7 @@ func main() {
 	//// Get all books
 	r.Use(cors.Default())
 	r.GET("/albums", FindAlbums)
-
+	r.DELETE("/albums/:id", DeleteAlbum)
 	r.Run()
 
 	//router := gin.Default()
@@ -40,4 +41,23 @@ func FindAlbums(c *gin.Context) {
 	var albums []model.Album
 	db.Find(&albums)
 	c.JSON(http.StatusOK, gin.H{"data": albums})
+}
+
+func DeleteAlbum(c *gin.Context) {
+	// Получение объекта базы данных из контекста запроса
+	db := c.MustGet("db").(*gorm.DB)
+	// Получение ID альбома из параметров запроса
+	id := c.Param("id")
+	// Выполнение операции удаления
+	// Создаем временный объект для хранения информации об удаляемом альбоме (для проверки существования)
+	var album []model.Album
+	if err := db.Where("id = ?", id).First(&album).Error; err != nil {
+		// Если альбом с таким ID не найден, отправляем клиенту ошибку 404
+		c.JSON(http.StatusNotFound, gin.H{"error": "Album not found"})
+		return
+	}
+	// Если альбом найден, производим его удаление
+	db.Delete(&album)
+	// Отправляем подтверждение об успешном удалении
+	c.JSON(http.StatusOK, gin.H{"data": true})
 }
