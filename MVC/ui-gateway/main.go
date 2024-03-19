205398767458e11dc2fc577ca0fb4cb35b4faa81
Egor Kovalev <egorkowalev1997@gmail.com>
2024-03-19 16:35:25 +0300

    Migrate to place for methods.

--- MVC/ui-gateway/main.go
+++ MVC/ui-gateway/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
-	"log"
 	"net/http"
+	metod "ui-gateway/controllers"
 	"ui-gateway/model"
 )
 
@@ -24,10 +23,10 @@ func main() {
 	//// GET /books
 	//// Get all books
 	r.Use(cors.Default())
-	r.GET("/albums", FindAlbums)
-	r.DELETE("/albums/:id", DeleteAlbum)
-	r.POST("/albums", AddAlbum)
-	r.PUT("/albums/:id", UpdateAlbum) // ИЛИ r.PATCH("/albums/:id", handlers.UpdateAlbum)
+	r.GET("/albums", metod.FindAlbums)
+	r.DELETE("/albums/:id", metod.DeleteAlbum)
+	r.POST("/albums", metod.AddAlbum)
+	r.PUT("/albums/:id", metod.UpdateAlbum) // ИЛИ r.PATCH("/albums/:id", handlers.UpdateAlbum)
 	r.Run()
 
 	//router := gin.Default()
@@ -35,92 +34,4 @@ func main() {
 	//
 	//router.Run("localhost:8080")
 
-}
-
-// GET /books
-// Get all books
-func FindAlbums(c *gin.Context) {
-	db := c.MustGet("db").(*gorm.DB)
-	var albums []model.Album
-	db.Find(&albums)
-	c.JSON(http.StatusOK, gin.H{"data": albums})
-}
-
-func DeleteAlbum(c *gin.Context) {
-
-	log.Println("попытка запуска DeleteAlbum!")
-	// Получение объекта базы данных из контекста запроса
-	db := c.MustGet("db").(*gorm.DB)
-	// Получение ID альбома из параметров запроса
-	id := c.Param("id")
-	// Выполнение операции удаления
-	// Создаем временный объект для хранения информации об удаляемом альбоме (для проверки существования)
-	var album model.Album
-	if err := db.Where("id = ?", id).First(&album).Error; err != nil {
-		// Если альбом с таким ID не найден, отправляем клиенту ошибку 404
-		c.JSON(http.StatusNotFound, gin.H{"error": "Album not found"})
-		return
-	}
-	// Если альбом найден, производим его удаление
-	db.Delete(&album)
-	// Отправляем подтверждение об успешном удалении
-	c.JSON(http.StatusOK, gin.H{"data": true})
-}
-
-// AddAlbum добавляет новый альбом в базу данных.
-func AddAlbum(c *gin.Context) {
-	// Получение объекта базы данных из контекста запроса
-	db := c.MustGet("db").(*gorm.DB)
-
-	// Создание объекта для нового альбома
-	var newAlbum model.Album
-
-	// Пробуем получить данные альбома из тела запроса
-	if err := c.ShouldBindJSON(&newAlbum); err != nil {
-		// В случае ошибки возвращаем сообщение об ошибке клиенту
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Добавление нового альбома в базу данных
-	if err := db.Create(&newAlbum).Error; err != nil {
-		// В случае ошибки при добавлении записи возвращаем ошибку сервера
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Возвращаем подтверждение об успешном добавлении
-	c.JSON(http.StatusCreated, gin.H{"data": newAlbum})
-}
-
-// UpdateAlbum обновляет существующий альбом в базе данных.
-func UpdateAlbum(c *gin.Context) {
-	// Получение объекта базы данных из контекста запроса
-	db := c.MustGet("db").(*gorm.DB)
-
-	// Получение ID альбома из параметров URL
-	id := c.Param("id")
-
-	// Проверяем, существует ли альбом с таким ID
-	var album model.Album
-	if err := db.Where("id = ?", id).First(&album).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Album not found"})
-		return
-	}
-
-	// Получаем обновленные данные альбома из тела запроса
-	var updatedData model.Album
-	if err := c.ShouldBindJSON(&updatedData); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Обновляем альбом в базе данных
-	if err := db.Model(&album).Updates(updatedData).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Отправляем подтверждение об успешном обновлении
-	c.JSON(http.StatusOK, gin.H{"data": album})
 }
